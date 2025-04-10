ec088b0ac54ba370f61f52b149ea13a49751c387
Marvin <marvin@kurock.io>
2025-04-10 10:11:51 +0200

    update

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+#! /usr/bin/env python
+import zipfile
 from io import BytesIO
+from datetime import datetime
+import os
 import sys
 import base64
 import re
@@ -11,6 +15,11 @@ from PIL import Image
 
 model = None
 class_names = ['AD', 'GND', 'VAD']  # Your class names
+chart_types = {
+  "AD": "airport",
+  "VAD": "arrival",
+  "GND": "taxi"
+}
 
 def print_progress(percent):
     percent = round(percent)
@@ -20,11 +29,10 @@ def print_progress(percent):
 baseurl = "https://aip.dfs.de/BasicVFR/pages/"
 debug = False
 
-def predict(bytes: bytes):
+def predict(image):
   print("predicting")
-  image = Image.open(BytesIO(bytes))
-  image = image.resize((256, 256))
-  img_array = np.array(image)
+  resized = image.resize((256, 256))
+  img_array = np.array(resized)
 
   # Check if the image has 3 channels (RGB)
   if img_array.shape[-1] != 3:
@@ -70,7 +78,7 @@ def download():
       ap_url = urljoin(baseurl, url)
       c2 = requests.get(ap_url)
       hrefs = re.findall(r'href=["\'](\.\./pages/[^"\']*)["\']', c2.text)
-      for i, u in enumerate(hrefs):
+      for u in hrefs:
         cu = urljoin(ap_url, u)
 
         c3 = requests.get(cu)
@@ -80,18 +88,44 @@ def download():
           src = match.group(1)
           b64 = src.replace("data:image/png;base64,", "")
           binary = base64.b64decode(b64)
-          t = predict(binary)
+          img = Image.open(BytesIO(binary))
+          t = predict(img)
           print(t)
-          suffix = "" if type_counter[t] == 0 else type_counter[t] 
-          with open(f"charts/{code}_airport_{t}{suffix}.png", "wb") as f:
-            f.write(binary)
-            type_counter[t] = type_counter[t] + 1
+          type_counter[t] = type_counter[t] + 1
+          suffix = "" if type_counter[t] == 1 else type_counter[t] 
+          out_name = f"charts/byop/{code}_{chart_types[t]}_{t}{suffix}.pdf"
+          img.save(out_name)
+
         else:
           print("Image with id 'imgAIP' not found.")
     else:
       print("URL not found.")
       continue
 
+def update_manifest():
+  tpl = None
+  with open('manifest.json') as f:
+    tpl = json.load(f)
+  v = tpl['version']
+  v = round(v+0.1, 1)
+  tpl['version'] = v
+  now = datetime.now().strftime('%Y%m%dT%H:%M:%SZ')
+  tpl['effectiveDate'] = now
+  print(tpl)
+  with open('charts/manifest.json', 'w') as f:
+    json.dump(tpl, f)
+  with open('manifest.json', 'w') as f:
+    json.dump(tpl, f)
+
+  return tpl
+def create_zip(manifest):
+  f_name = f'mk_aip_bundle_{manifest["version"]}.zip'
+  with zipfile.ZipFile(f_name, 'w') as zf:
+    for root, _, files in os.walk('charts'):
+      for file in files:
+        fp = os.path.join(root, file)
+        arcname = fp
+        zf.write(fp, arcname)
 
 def main():
   args = sys.argv
@@ -100,10 +134,24 @@ def main():
     print("this is main")
     exit(0)
 
-  if args[1] == "download":
+  if args[1] == 'debug':
+    m = update_manifest()
+    create_zip(m)
+
+  if args[1] == "new":
     global model 
+    files = [f for f in os.listdir('charts/byop/')]
+    for f in files:
+      os.remove('charts/byop/'+f)
+    try:
+      os.remove('charts/manifest.json')
+    except:
+      pass
     model = tf.keras.models.load_model('./model-1.0.keras')
     download()
+    m = update_manifest()
+    create_zip(m)
+
 
 if __name__ == "__main__":
   main()
